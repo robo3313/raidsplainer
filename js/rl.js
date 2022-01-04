9865a5c6bd09cb26ce8b2e9da613a84505455121
Romain Bonneil <late2201@gmail.com>
2022-01-04 04:57:46 +0100

    finished platform 1

--- js/rl.js
+++ js/rl.js
@@ -4,12 +4,15 @@ function RaidLeading (ctx, time) {
     //Objects set by user
     let planImage = null;
     let items = null;
+    let abilities = null;
     let events = null;
 
     //To keep track of time
     let currentEvent = 0;
     let currentTimer = null;
+    let elapsedTimer = null;
     let lastTimer = null;
+    let animation = true;
 
     //Internal js shenanigans
     let interval = null;
@@ -28,18 +31,37 @@ function RaidLeading (ctx, time) {
         }
     }
 
+    _myLibraryObject.setAbilities = function (newAbilities) {
+        abilities = newAbilities;
+        for (const i in abilities) {
+            abilities[i].image = document.getElementById(i);
+        }
+        console.log(abilities);
+    }
+    
     _myLibraryObject.setEvents = function (newEvents) {
         events = newEvents;
     }
 
-    _myLibraryObject.animate = function() {
+    _myLibraryObject.start = function() {
         interval = setInterval(loop, time);
     }
 
+    _myLibraryObject.pause = function() {
+        animation = false;
+    }
+
+    _myLibraryObject.resume = function() {
+        lastTimer = performance.now();
+        animation = true;
+    }
+
     function loop() {
-        currentTimer = performance.now();
-        const elapsedTimer = currentTimer - lastTimer;
-        lastTimer = currentTimer;
+        elapsedTimer = performance.now() - lastTimer;
+        if (animation) {
+            currentTimer += elapsedTimer;
+        }
+        lastTimer = performance.now();
         if (currentEvent >= events.length - 1) {
             clearInterval(interval);
             return ;
@@ -51,6 +73,7 @@ function RaidLeading (ctx, time) {
 
     function calcEvent(currentTimer) {
       const secondsSinceStart = Math.floor(currentTimer / 1000);
+      document.getElementById("timer").innerHTML = secondsSinceStart;
       if (events[currentEvent].time < secondsSinceStart) {
         currentEvent += 1;
         triggerEvent(events[currentEvent]);
@@ -60,16 +83,17 @@ function RaidLeading (ctx, time) {
     function triggerEvent(event) {
         document.getElementById("event-title").innerHTML = event.name;
         document.getElementById("event-description").innerHTML = event.description;
-        calcTargets(event);
-    }
-
-
-    function calcTargets(event) {
-      if (event.positions !== undefined) {
-        for (const i in items) {
-            items[i].target = event.positions[i];
+        if (event.positions !== undefined) {
+            for (const i in items) {
+                items[i].target = event.positions[i];
+            }
         }
-      }
+        if (event.teleport !== undefined) {
+            for (const i in items) {
+                items[i].position.x = event.teleport[i].x;
+                items[i].position.y = event.teleport[i].y;
+            }
+        }
     }
 
     function calcPositions(elapsedTimer) {
@@ -91,16 +115,28 @@ function RaidLeading (ctx, time) {
     }
 
     function drawIcons(ctx) {
-      ctx.drawImage(planImage, 0, 0);
-      for (const i in items) {
-        ctx.drawImage(items[i].image, items[i].position.x - items[i].image.width / 2, items[i].position.y - items[i].image.height / 2);
-        writeText(ctx, i, items[i].position.x, items[i].position.y);
-      }
-      if (events[currentEvent].drawings !== undefined) {
-          if (events[currentEvent].drawings[0].type === "circle") {
-              drawCircle(ctx, events[currentEvent].drawings[0].x, events[currentEvent].drawings[0].y, events[currentEvent].drawings[0].r);
-          }
-      }
+        ctx.drawImage(planImage, 0, 0);
+        for (const i in items) {
+            ctx.drawImage(items[i].image, items[i].position.x - items[i].image.width / 2, items[i].position.y - items[i].image.height / 2);
+            writeText(ctx, i, items[i].position.x, items[i].position.y);
+        }
+        const event = events[currentEvent];
+        if (event.drawings !== undefined) {
+            for (const i in event.drawings) {
+                const drawing = event.drawings[i];            
+                if (drawing.type === "circle") {
+                    const color = drawing.color !== undefined ? drawing.color : "black";
+                    drawCircle(ctx, drawing.x, drawing.y, drawing.r, color);
+                }
+            }
+        }
+        if (event.abilities !== undefined) {
+            for (const i in event.abilities) {            
+                const ability = event.abilities[i];
+                ctx.drawImage(abilities[ability.id].image, ability.x - abilities[ability.id].image.width / 2, ability.y - abilities[ability.id].image.height / 2);
+                writeText(ctx, ability.name, ability.x, ability.y);
+            }
+        }
     }
 
     function distance(startX, startY, endX, endY) {
@@ -120,10 +156,23 @@ function RaidLeading (ctx, time) {
       ctx.stroke();
     }
 
-    function drawCircle(ctx, x, y, r) {
+    function drawCircle(ctx, x, y, r, color) {
       ctx.beginPath();
       ctx.arc(x, y, r, 0, 2 * Math.PI);
+      ctx.fillStyle = color; 
+      ctx.fill();
       ctx.stroke();
+    }
+
+    function drawGradiant(x, y, r, x1, y1, r1) {
+        // Create gradient
+        var grd = ctx.createRadialGradient(75,50,5,90,60,100);
+        grd.addColorStop(0,"black");
+        grd.addColorStop(1,"white");
+
+        // Fill with gradient
+        ctx.fillStyle = grd;
+        ctx.fillRect(10,10,150,150);
     }
 
     function writeText(ctx, text, x, y) {
